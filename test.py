b3d22601829c5571953165169ba040ac2bd6021f
Ramstein <gerialworld@gmail.com>
2021-01-11 02:32:31 +0530

    code changes made by Ramstein

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 
 
 # for removing the python
-sudo apt-get remove --purge python2.7
-sudo apt-get remove --purge python3.6
+# sudo apt-get remove --purge python2.7
+# sudo apt-get remove --purge python3.6
